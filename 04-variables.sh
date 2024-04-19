ea4059c26ea87834ec3a21143a96a3fa2a960b9a
raj <raj.pradeep676@gmail.com>
2024-04-19 20:05:33 +0530

    variable

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+
+person1=$1
+person2=$2
+
+echo "$person1 : : hi sajay how are you?"
+echo  "$person2 : : im fine $person1 how are you?"
+echo "$person1: :im good, $person2 thank you"
+echo "$person2: : thank you"
