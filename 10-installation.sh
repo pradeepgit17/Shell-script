9a349e1bf4f869816b9bbb7f1072b169b1c90a34
raj <raj.pradeep676@gmail.com>
2024-04-19 20:42:13 +0530

    installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -13,3 +13,13 @@ echo "you are a super user"
 fi
 
 dnf install mysql-server -y
+
+if [ $? -ne 0 ]
+
+then 
+echo "installation of mysql is fails"
+exit 1
+else
+echo "installation of mysql is success"
+
+fi
