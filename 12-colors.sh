65db7bfa834ba5578db5d96b749b9a2b50da3419
raj <raj.pradeep676@gmail.com>
2024-04-20 08:10:38 +0530

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -2,6 +2,16 @@
 
 USERID=$(id -u)
 
+VALIDATE(
+if [ $1 -ne 0 ]
+then
+echo "$2 installation is fails"
+exit 1
+else
+echo "$2 installation is pass"
+fi 
+
+)
 if [ $USERID -ne 0 ]
 then
 echo "please run the scrpits with root access"
@@ -13,4 +23,8 @@ echo "your are super user"
 fi
 
 dnf install mysql-server -y
+
+VALIDATE $? "installation mysql"
+
+
 
