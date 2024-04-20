f19b3d5ae08f22ccde29e9789c3742da7560744f
raj <raj.pradeep676@gmail.com>
2024-04-20 09:00:48 +0530

    instal-packages

--- 14-installationpackages.sh
+++ 14-installationpackages.sh
@@ -6,17 +6,18 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-    echo "$2 installation is fails"
+    echo -e "$2 installation is fails"
     exit 1
     else
-    echo "$2 installation is success"
+    echo -e  "$2 installation is success"
     fi
-}
+      }
 echo "script started execting at : $TIMESTAMP"
 if [ $USERID -ne 0 ]
 then
