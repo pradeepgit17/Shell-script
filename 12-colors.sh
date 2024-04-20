c024d74e2edfb82b68c15811c4ba96ab61b9cd88
raj <raj.pradeep676@gmail.com>
2024-04-20 08:02:35 +0530

    colors

---
+++ 12-colors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+echo "please run the scrp its with root access"
+exit 1
+else 
+echo "your are super user"
+
+fi
+
+dnf install mysql-server -y
+
